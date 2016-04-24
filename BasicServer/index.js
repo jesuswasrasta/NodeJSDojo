6b241827c047f211c03033b43c460d343df14162
jesuswasrasta <jesus_was_rasta@yahoo.it>
2016-04-24 09:45:50 +0200

    Implements a request handler
    * inject the request handler to decouple it from router
    * using a javascript object to define routes

--- BasicServer/index.js
+++ BasicServer/index.js
@@ -1,4 +1,14 @@
 var server = require("./server");
 var router = require("./router");
+var requestHandlers = require("./requestHandlers.js")
 
-server.start(router.route);
+//This is a javascript object
+//In js, objects are collection of key-value pairs, like dictionaries;
+//the key is a string, the value can be string, number or a function!
+//So, handle is a collection of request handlers
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
