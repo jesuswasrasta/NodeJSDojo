b308b3a625c8a7025ac41da2fe5b786198ec1108
jesuswasrasta <jesus_was_rasta@yahoo.it>
2016-04-24 11:02:33 +0200

    Introducing event listeners
    * how to handle POST requests
    * the "data" and "end" events
    * Node.js gather posted data in chuncks

--- BasicServer/requestHandlers.js
+++ BasicServer/requestHandlers.js
@@ -1,18 +1,22 @@
-var exec = require("child_process").exec;
-
 function start(response){
     console.log("Request handler 'start' was called.");
         
-    /* Try to make a more time-consuming call to verify 
-    upload() function will not be affected
-    */
-    //exec("find \"C:\goofy.txt\"", function(error, stdout, stderr) {
-    //exec("ls -lah", function(error, stdout, stderr) {
-    exec("dir /B /S /W", function(error, stdout, stderr) {
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(stdout);
-        response.end();
-    })
+    var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+     response.writeHead(200, {"Content-Type": "text/html"});
+     response.write(body);
+     response.end();
 }
 
 function upload(response) {
