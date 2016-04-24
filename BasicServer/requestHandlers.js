84c26dba4703776e36095566f32d6a2456d332d1
jesuswasrasta <jesus_was_rasta@yahoo.it>
2016-04-24 10:01:41 +0200

    how how blocking calls work on Node.js
    * even if blocking operation is in start() function of
      request handler, even upload() function suffers the
      blocking computation delay

--- BasicServer/requestHandlers.js
+++ BasicServer/requestHandlers.js
@@ -1,5 +1,14 @@
 function start(){
     console.log("Request handler 'start' was called.");
+    
+    //Simulating a blocking call
+    function sleep(milliSeconds) {
+        var startTime = new Date().getTime();
+        while (new Date().getTime() < startTime + milliSeconds);
+    }
+    
+    sleep(10000);
+    
     return "Hello Start";
 }
 
